9c0795cbd8e55dc54e8e9c6672f35707de1d04fa
Hugo <hugovinicius145@gmail.com>
2022-03-06 22:13:03 -0300

    Autenticação nas rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import { ensureAutheticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAutheticated);
 specificationsRoutes.post('/', createSpecificationController.handle);
 
 export { specificationsRoutes }
