54abe4e63cdea77b9b633ee62f425718bf199c43
Hugo <hugovinicius145@gmail.com>
2022-03-09 10:51:11 -0300

    Upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import uploadConfig from '../config/upload';
+import { ensureAutheticated } from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
-usersRoutes.patch('/avatar', updateUserAvatarController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAutheticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes }
